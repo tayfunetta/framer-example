9dfb2701b7c9ee246f176f76828d09b361d078ec
tayfunetta <tayfunemrekahraman@gmail.com>
2023-10-01 23:34:52 +0300

    initial commit for hover branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import { motion, animate } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen text-white pt-20">
       <motion.h1
         className="text-center text-4xl"
-        animate={{ fontSize: "52px", color: "hsl(120, 100, 50)", x: 100, y: 100 }}
+        animate={{ fontSize: "52px", y: 100 }}
       >
-        Hello World HOO HOO
+        Hello World Hover
       </motion.h1>
     </div>
   );
