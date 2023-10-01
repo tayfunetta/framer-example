1a03123cf28aa1b5690df027bd90fb28a0cf44ed
tayfunetta <tayfunemrekahraman@gmail.com>
2023-10-01 18:59:37 +0300

    lesson2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { motion, animate } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
         className="text-center text-4xl"
         animate={{ fontSize: "52px", color: "hsl(120, 100, 50)", x: 100, y: 100 }}
       >
-        Hello World HOO HOO
+        Hello World lesson 2
       </motion.h1>
     </div>
   );
