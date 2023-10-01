5b82e962e7f862fd94d255b77f9330a795949f1f
tayfunetta <tayfunemrekahraman@gmail.com>
2023-10-01 23:23:51 +0300

    transition options

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,23 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen text-white pt-20">
-      <motion.h1
-        className="text-center text-4xl"
-        animate={{ fontSize: "52px", color: "hsl(120, 100, 50)", x: 100, y: 100 }}
+    <div className="bg-black h-screen text-white overflow-hidden">
+      <motion.button
+        className="block mx-auto px-10 py-4 bg-blue-600 rounded-xl"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 2 }}
       >
-        Hello World lesson 2
-      </motion.h1>
+        button
+      </motion.button>
+      <motion.p
+        className="text-center text-lg"
+        initial={{ y: 700 }}
+        animate={{ y: 200 }}
+        transition={{ type:"spring", stiffness: 120 }}
+      >
+        Qui nulla dolor eu labore adipisicing officia irure nulla.
+      </motion.p>
     </div>
   );
 }
