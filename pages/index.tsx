1cbf5b8b2127ddf8a2921d19c8eabf7d9b322f25
tayfunetta <tayfunemrekahraman@gmail.com>
2023-10-02 14:01:22 +0300

    first commit of variants

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,43 @@
 import { motion } from "framer-motion";
 
+const buttonVariants = {
+  initial: {
+    opacity: 0,
+    y: -100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const pVariants = {
+  initial: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+  },
+};
+
 export default function Home() {
   return (
     <div className="bg-black h-screen text-white overflow-hidden">
       <motion.button
         className="block mx-auto px-10 py-4 bg-blue-600 rounded-xl"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1, duration: 2 }}
+        variants={buttonVariants}
+        initial={"initial"}
+        animate={"visible"}
+        transition={{ delay: 1, duration: 1 }}
       >
         button
       </motion.button>
       <motion.p
-        className="text-center text-lg"
-        initial={{ y: 700 }}
-        animate={{ y: 200 }}
-        transition={{ type:"spring", stiffness: 120 }}
+        className="text-center text-lg mt-52"
+        variants={pVariants}
+        initial={"initial"}
+        animate={"visible"}
+        transition={{ type: "spring", stiffness: 120 }}
       >
         Qui nulla dolor eu labore adipisicing officia irure nulla.
       </motion.p>
