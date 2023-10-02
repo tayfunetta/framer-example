d02d0efb034315c2807d487d09f20dd947b9cfc7
tayfunetta <tayfunemrekahraman@gmail.com>
2023-10-03 00:11:35 +0300

    added loop when hovering

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,91 +2,40 @@ import { motion } from "framer-motion";
 
 const buttonVariants = {
   initial: {
-    opacity: 0,
     y: -100,
   },
   visible: {
-    opacity: 1,
     y: 50,
-    transition: { delay: 1, duration: 1 },
-    // specify the "transition" property within "visible" property
-  },
-  hover: {
-    scale: 1.1,
-    textShadow: "0px 0px 8px rgb(255,255,255)",
-    boxShadow: "0px 0px 8px rgb(255,255,255)",
   },
 };
-
-const pVariants = {
-  initial: {
-    x: "-100vw",
-  },
-  visible: {
-    x: 0,
-    transition: { type: "spring", stiffness: 120 },
-    // specify the "transition" property within "visible" property
-  },
-};
-
-const ulVariants = {
-  initial: {
-    y: "-100vh",
-  },
-  visible: {
-    y: 0,
-    transition: { staggerChildren: 0.1, when: "beforeChildren" },
-  },
-};
-
-const liVariants = {
-  initial: {
-    opacity: 0,
-  },
-  visible: {
-    opacity: 1,
-  },
-};
+// YOU CAN NOT SPECIFY 'REPEATTYPE' property in variants
+// this will throw an error
+// specify repeatType in whileHover property instead.
 
 export default function Home() {
   return (
     <div className="bg-black h-screen text-white overflow-hidden">
       <motion.button
-        className="block mx-auto px-10 py-4 bg-blue-600 rounded-xl"
+        className="block mx-auto px-10 py-10 bg-blue-600 rounded-xl"
         variants={buttonVariants}
         initial={"initial"}
         animate={"visible"}
-        whileHover="hover"
+        
+        // use repeat properties to loop animation
+        // make sure to specify hover property inline so as to prevent the app throwing an error
+        whileHover={{
+          scale: 1.2,
+          textShadow: "0px 0px 8px rgb(255,255,255)",
+          boxShadow: "0px 0px 8px rgb(255,255,255)",
+          transition: {
+            repeat: Infinity,
+            repeatType: "reverse",
+            duration: 0.15,
+          },
+        }}
       >
         button
       </motion.button>
-      <motion.p
-        className="text-center text-lg mt-52"
-        variants={pVariants}
-        initial={"initial"}
-        animate={"visible"}
-      >
-        Qui nulla dolor eu labore adipisicing officia irure nulla.
-      </motion.p>
-
-      <motion.ul
-        className="w-80 mx-auto"
-        variants={ulVariants}
-        initial="initial"
-        animate="visible"
-      >
-        <motion.li variants={liVariants}>aaa</motion.li>
-        <motion.li variants={liVariants}>aaa</motion.li>
-        <motion.li variants={liVariants}>aaa</motion.li>
-        <motion.li variants={liVariants}>aaa</motion.li>
-        <motion.li variants={liVariants}>aaa</motion.li>
-      </motion.ul>
-
-      <motion.div
-        className="mt-[7rem] bg-teal-600 w-36 h-36 rounded-full"
-        animate={{ x: 700 }}
-        transition={{ type: "spring", damping: 13 }}
-      />
     </div>
   );
 }
