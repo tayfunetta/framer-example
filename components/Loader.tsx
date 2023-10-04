eb995d2b8916e3d3785ad332a1180f8dd02216c4
tayfunetta <tayfunemrekahraman@gmail.com>
2023-10-04 22:33:04 +0300

    added cycle hook

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,22 +1,37 @@
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
-const loaderVariant = {
-  visible: {
-    x: [0, -20, 20],
-    // y: [0, -20, 20],
-    transiton: {
-      x: { repeat: Infinity, repeatType: "reverse" },
+const loaderVariants = {
+  animationOne: {
+    x: [-20, 20],
+    y: [0, 30],
+    transition: {
+      x: { duration: 0.5, repeat: Infinity, repeatType: "reverse" },
+      y: { duration: 0.25, repeat: Infinity, repeatType: "reverse" },
+    },
+  },
+  animationTwo: {
+    y: [0, 30],
+    x: 0,
+    transition: {
+      y: { duration: 0.25, repeat: Infinity, repeatType: "reverse" },
     },
   },
 };
 
 function Loader() {
+  const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
+
   return (
-    <motion.div
-      className="w-3 h-3 mx-auto bg-white rounded-full"
-      variants={loaderVariant}
-      animate="visible"
-    />
+    <>
+      <motion.div
+        className="w-3 h-3 mx-auto bg-white rounded-full"
+        variants={loaderVariants}
+        animate={animation}
+      />
+      <button className="bg-white text-black px-4 py-2 rounded-lg" onClick={() => cycleAnimation()}>
+        asdasda
+      </button>
+    </>
   );
 }
 
