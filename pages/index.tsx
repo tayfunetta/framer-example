7a2d247276d65437c2a1504779c166216126d470
tayfunetta <tayfunemrekahraman@gmail.com>
2023-10-04 22:57:12 +0300

    dragging first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import { motion, animate } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen text-white pt-20">
-      <motion.h1
-        className="text-center text-4xl"
-        animate={{ fontSize: "52px", color: "hsl(120, 100, 50)", x: 100, y: 100 }}
-      >
-        Hello World HOO HOO
-      </motion.h1>
+      <h1 className="text-center text-4xl">Hello World HOO HOO</h1>
+      <motion.div
+        className="w-36 h-36 mx-auto bg-emerald-500 rounded-lg"
+        drag
+        dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+        dragElastic={0.3}
+      />
     </div>
   );
 }
